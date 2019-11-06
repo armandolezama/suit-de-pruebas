e2c1da37aa083f3c8a1b3f4a0d815e45266c1abf
Armando Lezama <armando.lezama.psic@gmail.com>
2019-11-06 12:42:33 -0600

    Se queda pendiente el grid

--- src/sudoku/sudoku.js
+++ src/sudoku/sudoku.js
@@ -4,22 +4,26 @@ const exploreArray = (minorArray) => {
     if(minorArray.length !== arraySet.size){
         return "Is a invalid case"
     }
+
+    return "";
 }
 
 const exploreColumn = (array) => {
     let compareArray = [];
     let validCase = "";
 
-    for(const index in array){
-        column = index
-        for(const row of array[column]){
+    for(const column in array){
+        for(const row in array[column]){
             compareArray.push(array[row][column]);
         }
-        validCase = exploreArray(compareArray)
+        validCase = exploreArray(compareArray);
+        compareArray = [];
         if([...validCase].length > 0 ){
             return validCase
         }
     }
+
+    return "";
 
 }
 
@@ -30,23 +34,33 @@ const exploreRow = (array) => {
         validCase = exploreArray(element)
 
         if([...validCase].length > 0 ){
+            console.log('exploreRow')
             return validCase
         }
     }
 
-    return validCase;
+    return "";
 }
 
-const startByRow = (array, stack) => {
-    
+
+const getRowsByGrid = (array, gridRow) => {
+    let newArray = array.slice(gridRow - 3, gridRow)
+    return newArray
 }
+
+const getOneGrid = (array, gridCol) => {
+    let newGrid = []
+    for(const gridRow of array){
+        newGrid.push(gridRow.slice(gridCol -3, gridCol))
+    }
+    return newGrid
+}
+
 
 const exploreGrid = (array) => {
-
-    array.slice(0, 2)
-    array.slice(3, 5)
-    array.slice(6, 8)
-
+    let grids = [];
+    grids.push(getOneGrid(getRowsByGrid(array, 3) , 3));
+    return grids
 }
 
 const evaluateSudoku = (array) => {
